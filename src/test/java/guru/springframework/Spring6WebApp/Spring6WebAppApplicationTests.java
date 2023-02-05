6b34550a83f21a417a8bc92ea857896f4f9e4f04
Donald F. Coffin <dcoffin@greenbuttonalliance.org>
2023-02-04 21:44:36 -0500

    Initial commit

---
+++ src/test/java/guru/springframework/Spring6WebApp/Spring6WebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.Spring6WebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6WebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
