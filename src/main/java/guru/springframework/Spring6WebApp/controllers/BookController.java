89b5d981209ddc202f2fb6577fcb63e27ce84dcc
Donald F. Coffin <dcoffin@greenbuttonalliance.org>
2023-02-05 15:37:39 -0500

    Add Book Controller

---
+++ src/main/java/guru/springframework/Spring6WebApp/controllers/BookController.java
@@ -0,0 +1,27 @@
+package guru.springframework.Spring6WebApp.controllers;
+
+import guru.springframework.Spring6WebApp.services.BookService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author Donald F. Coffin
+ */
+@Controller
+public class BookController {
+
+	private final BookService bookService;
+
+	public BookController(BookService bookService) {
+		this.bookService = bookService;
+	}
+
+	@RequestMapping(value = "/books")
+	public String getBooks(Model model) {
+
+		model.addAttribute("books", bookService.findAll());
+
+		return "books";
+	}
+}
